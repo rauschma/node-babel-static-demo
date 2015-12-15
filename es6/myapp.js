b9071bf8d2e924c8f0131616fd12c7f3b025e931
rauschma <axel@rauschma.de>
2015-12-15 13:08:43 +0100

    Added and used babel-polyfill

--- es6/myapp.js
+++ es6/myapp.js
@@ -1,6 +1,8 @@
 import { install } from 'source-map-support';
 install();
 
+import 'babel-polyfill';
+
 console.log([1,2,3].map(x => x * x));
 
-throw new Error('Test!');
+throw new Error('Test!'); // line 8
